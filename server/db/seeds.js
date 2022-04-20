bde226e2218766b4b0eac5b0dccaad4b588cf104
Jack <jackowendickson@gmail.com>
2022-04-20 10:52:59 +0100

    seeds file done

---
+++ server/db/seeds.js
@@ -0,0 +1,17 @@
+use happy_hotel
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Steve Jobs",
+        email_address: "stevejobs@apple.com",
+        // check_in_date: "2022-05-02",
+        // check_out_date: "2022-05-07",
+        checked_in: true
+    },
+    {
+        name: "Bill Gates",
+        email_address: "billgate@microsoft.com",
+        checked_in: false
+    }
+]);
